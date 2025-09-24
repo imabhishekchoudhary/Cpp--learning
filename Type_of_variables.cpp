3d96e048bcf7c2f82c7e80b854e9f900beab5a8d
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-09-24 18:40:45 +0530

    initial commit

---
+++ Type_of_variables.cpp
@@ -0,0 +1,29 @@
+/*Variable type int,float,double,char,Boolean.
+the based on scope,variable can be clssiifuel into two type:
+1.local variable:- Local variable are declared inside the braces of any function and can be accessed only from there.
+2.global variable:- Global variable are declared outsid any function and can be accessed from anywhere.
+
+Scope = is the using in c++ he is the valid code existence
+
+Data type three types
+1.Built-in:- int,float,char etc.
+2.User-defined:- struct,union,enum.
+3.Derived:- array,function,pointer.
+*/
+
+ #include<iostream>
+
+ int main(){
+    //int a=12;
+    //int b=13;
+    int a=12, b=13;
+    int sum =a+b;
+    float f=23.3;
+    char c='a';
+    std::cout<<"sum of a&b:"<<sum<<"\nthe float is:"<<f<<"\nchar is:"<<c;
+
+ }
+
+ /*variable name in c++ in range is 1 to 255 characters.
+ 
+ */
---
+++ Variables_comments.cpp
@@ -0,0 +1,8 @@
+/*variable is a container to store your data*/
+#include<iostream>
+//using namespace std;
+int main(){
+    int a=33;
+    std::cout<<a;//not write std::
+    return 0;
+}
